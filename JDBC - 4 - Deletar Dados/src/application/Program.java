baab0ab08bb0d040341e3e028fb3988d00e4d2f3
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-03-11 12:55:18 -0300

    Seção transações

--- JDBC - 4 - Deletar Dados/src/application/Program.java
+++ JDBC - 4 - Deletar Dados/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import db.DB;
 import db.DBException;
+import db.DBIntegrityException;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -16,25 +17,23 @@ public class Program {
         try{
             conn = DB.getConnection();
             st = conn.prepareStatement(
-                    "UPDATE seller "
-                    + "SET BaseSalary = BaseSalary + ? "
+                    "DELETE FROM department "
                     + "WHERE "
-                    + "(DepartmentId = ?)");
+                    + "Id >= ?");
 
-            st.setDouble(1, 200.0);
-            st.setInt(2, 2);
+            st.setInt(1, 20);
+
 
             int rowsAffected = st.executeUpdate();
+            System.out.println("Done! Affected rows: " + rowsAffected);
 
-            System.out.println("Done! Updated rows: " + rowsAffected);
-        } catch (SQLException e){
-            throw new DBException(e.getMessage());
-        }finally {
+
+
+        }catch (SQLException e){
+            throw new DBIntegrityException(e.getMessage());
+        } finally {
             DB.closeStatement(st);
             DB.closeConnection();
         }
-
-
     }
-
 }
